11425baf2541817caa83103fc5376f56b347e63e
bautistam1 <147621284+bautistam1@users.noreply.github.com>
2024-09-06 10:39:03 -0300

    Initial commit

---
+++ Assets/CharacterAgente.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAgente : MonoBehaviour
+{
+    public NavMeshAgent navMesh;
+        public Animator anim;
+    public Transform targetTR;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMesh.destination = targetTR.position;
+        anim.SetFloat("Speed", navMesh.velocity.magnitude);
+    }
+}
---
+++ Assets/Gorro.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gorro : MonoBehaviour
+{
+    public GameObject gorroMalo;
+    public GameObject gorroBueno;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemigo"))
+        {
+            gorroBueno.SetActive(!gorroBueno.activeInHierarchy);
+            gorroMalo.SetActive(!gorroMalo.activeInHierarchy);
+        }
+       
+    }
+
+
+    
+}
